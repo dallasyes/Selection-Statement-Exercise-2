d7906be377e53fcce65f9633fa047efcaa2d55ee
Dallas Yu <dallas_yu@hotmail.com>
2025-06-24 16:59:03 -0500

    added favorite subject question

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+            switch (favoriteSubject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is cool because it's all about solving problems!");
+                    break;
+                case "Biology":
+                    Console.WriteLine("Biology is cool because it's all about solving problems");
+                    break;
+                case "History":
+                    Console.WriteLine("History is cool because it's all about solving problems");
+                    break; 
+                case "Social Studies":
+                    Console.WriteLine("Social Studies is cool because it's cool");
+                    break;
+                case "Gym":
+                    Console.WriteLine("Gym is cool because it's all about solving problems");
+                    break;
+                default:
+                    Console.WriteLine("Interesting choice! What do you like about it?");
+                    break;
+            }
         }
     }
 }
